57214c043ef6190573953e8566851b5bf707b53d
Shivam Mahajan <91623734+Shivam444444@users.noreply.github.com>
2026-04-08 00:27:28 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+import turtle
+  t=turtle.turtle()
+  t.speed(100)
+  turtle.bgcolor("black")
+   fot i in range (240):
+       t.color("cyan")
+       t.circle(i)
+       t.left(5)
+        turtle.done()
+        
